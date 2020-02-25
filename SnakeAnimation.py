8e86e7d7a406d37bb67c8eb5b149d19b93b717ca
Christy Ye <eragon1731@gmail.com>
2020-02-25 15:13:03 -0800

    adding animation scripts

--- SnakeAnimation.py
+++ SnakeAnimation.py
@@ -1,6 +1,6 @@
 import maya.cmds as mc
 
-def attachToFollicles(joints, ribbon_jnts, follicle_grp):
+def attachToFollicles(joints, ribbon_jnts, follicle_grp, ribbonShape):
 
     #something
     for i in range(len(ribbon_jnts)):
@@ -8,24 +8,46 @@ def attachToFollicles(joints, ribbon_jnts, follicle_grp):
         ribbon_jnt = ribbon_jnts[i]
         foll = follicle_grp[i]
 
-        tempnode = mc.createNode('closestPointOnSurface')
-
+        cpos = mc.createNode('closestPointOnSurface')
         decomp = mc.createNode('decomposeMatrix')
+        ribbonShape.wordSpace.connect(cpos.inputSurface)
+        driver_jnt.wordMatrix.connect(decomp.inputSurface)
 
-    # for x in range(0,len(ribbonJoints)):
-    #         driverJnt = pm.PyNode(joints[x])
-    #         ribbonJnt = pm.PyNode(ribbonJoints[x])
-    #         foll = follicles[x]
-    #         cpos = pm.createNode('closestPointOnSurface')
-    #         # decomposeMatrix node converts local space to world space for each joint
-    #         decomp = pm.createNode('decomposeMatrix')
-    #         ribbonShape.worldSpace.connect(cpos.inputSurface)
-    #         driverJnt.worldMatrix.connect(decomp.inputMatrix)
-    #         decomp.outputTranslate.connect(cpos.inPosition)
-    #         blend = pm.createNode('blendColors')
-    #         blend.color1R.set(foll.parameterU.get())
-    #         cpos.result.parameterU.connect(blend.color2R)
-    #         follicleGroup.stretchy.connect(blend.blender)
-    #         # output to follicle
-    #         blend.outputR.connect(foll.parameterU)
+        decomp.outputTranslate.connect(cpos.inputSurface)
+        blend = mc.createNode('blendColours')
+        blend.color1R.set(foll.paramterU.get())
+        cpos.result.parameterU.connect(blend.color2R)
+        follicle_grp.stretchy.connect(blend.blender)
 
+        blend.outputR.connect(foll.paramterU)
+
+
+
+def smartAttachtoMotionPath(curve, targets):
+    curveShape = curve
+
+    try:
+        curve.type()
+    except:
+        curve = mc.PyNode(curve)
+    if curve.type() == 'transform':
+        curveShape = curve.getShape()
+    elif curve.type() != 'nurbsCurve':
+        mc.error("First param must be NURBS curve")
+        return False
+    maxU = 0
+
+    for target in targets:
+        try:
+            target.name()
+        except:
+            target = mc.PyNode(target)
+
+        origParent = target.getParent()
+        offsetGroup = mc.group(em=1, n=target.name() + '_mpOffset')
+        mc.parent(offsetGroup, origParent)
+        pc = mc.parentConstraint(target, offsetGroup)
+        mc.delete(pc)
+
+        mp = mc.createNode('motionPath')
+        curveShape.worldSpace.connect(mp.geometryPath)
